ce1a3ba7fbbb71929860ef17483a42030f97602d
91950 <krishnamurali3250@gmail.com>
2025-03-25 10:20:08 +0530

    Initial Commit

--- StudentAdministration/src/main/java/com/org/StudentAdministration/controller/AuthController.java
+++ StudentAdministration/src/main/java/com/org/StudentAdministration/controller/AuthController.java
@@ -2,29 +2,23 @@ package com.org.StudentAdministration.controller;
 
 import com.org.StudentAdministration.entity.LoginRequest;
 import com.org.StudentAdministration.entity.Student;
-import com.org.StudentAdministration.entity.User;
 import com.org.StudentAdministration.repository.StudentRepository;
-import com.org.StudentAdministration.repository.UserRepository;
 import com.org.StudentAdministration.service.JWTService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
- 
 
     @Autowired
     private StudentRepository studentRepository;
@@ -34,7 +28,6 @@ public class AuthController {
 
     @Autowired
     private JWTService jwtService;
-
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody Student student) {
